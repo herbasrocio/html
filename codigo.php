bcccb6175c816fc079ccf53af9841a6334c6ac98
herbasrocio <136300285+herbasrocio@users.noreply.github.com>
2023-07-19 19:28:01 -0300

    php a corregir

---
+++ codigo.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<form action="codigo.php" method="post">
+    Usuario: <input type="text" name="usuario"><br>
+    Contrasena: <input type="text" name="contrasena"><br>
+    <input type="submit" value="Enviar">
+</form>
+</body>
+</html>
