b8b8e63ed9370e09abd022074fe9c16e92c986bd
herbasrocio <136300285+herbasrocio@users.noreply.github.com>
2023-07-19 21:20:40 -0300

    ultimos cambios

--- codigo.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<form action="codigo.php" method="post">
-    Usuario: <input type="text" name="usuario"><br>
-    Contrasena: <input type="text" name="contrasena"><br>
-    <input type="submit" value="Enviar">
-</form>
-</body>
-</html>
